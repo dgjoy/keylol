0ee02942ff2e8a7e6d1554829a477edd0932d4d0
Stackie Jia <stackia@keylol.com>
2015-05-04 01:28:34 +0800

    Initial commit with generated files by ASP.NET scaffolding system.

---
+++ Keylol/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Keylol.Startup))]
+namespace Keylol
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Keylol Main Repository
