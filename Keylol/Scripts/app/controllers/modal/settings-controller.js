f270ad1d6fd91b6c036a952553974a73f83cf5e2
Stackie Jia <stackia@keylol.com>
2015-08-31 21:28:43 +0800

    update settings modal window

--- Keylol/Scripts/app/controllers/modal/settings-controller.js
+++ Keylol/Scripts/app/controllers/modal/settings-controller.js
@@ -3,11 +3,18 @@
 
 	keylolApp.controller("SettingsController", [
 		"$scope", "close",
-		function($scope, close) {
+		function ($scope, close) {
 			$scope.cancel = function() {
 				close();
 			};
+			$scope.changepage = function (p) {
+			    $scope.settings_page = { 1: false, 2: false, 3: false, 4: false };;
+			    $scope.settings_page[p] = true;
+			    console.log($scope.settings_page);
+			};
 			$scope.yourVariable = "test";
+			$scope.settings_page = { 1: false, 2: false, 3: false, 4: false };;
+			$scope.settings_page[1] = true;
 		}
 	]);
 })();
