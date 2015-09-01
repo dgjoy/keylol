a81846899bdd8ebebb9430b2df2eb06f9f32c19a
Stackie Jia <stackia@keylol.com>
2015-09-02 01:27:15 +0800

    Complete settings window.

--- Keylol/Scripts/app/controllers/modal/settings-controller.js
+++ Keylol/Scripts/app/controllers/modal/settings-controller.js
@@ -2,12 +2,19 @@
 	"use strict";
 
 	keylolApp.controller("SettingsController", [
-		"$scope", "close",
-		function ($scope, close) {
+		"$scope", "close", "utils",
+		function($scope, close, utils) {
 			$scope.cancel = function() {
 				close();
 			};
 			$scope.page = "profiles";
+			$scope.uniqueIds = {};
+			for (var i = 0; i < 22; ++i) {
+				$scope.uniqueIds[i] = utils.uniqueId();
+			}
+			$scope.geetestId = utils.createGeetest("float", function(result, geetest) {
+
+			});
 		}
 	]);
 })();
