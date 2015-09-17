7cd1644e4573edacaad4de2a51aae3f18baf13b8
Stackie Jia <stackia@keylol.com>
2015-09-17 16:00:53 +0800

    WCF service workflow.

---
+++ Keylol/Services/SteamBotManager.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Keylol.Services.Contracts;
+
+namespace Keylol.Services
+{
+    public class SteamBotManager : ISteamBotManager
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
