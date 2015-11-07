5e03393f65c65966c5d5c67b96a0ded985f412dc
Stackie Jia <stackia@keylol.com>
2015-11-07 21:08:21 +0800

    实现获取用户订阅的据点接口。

--- Keylol/Controllers/UserPointSubscriptionController.cs
+++ Keylol/Controllers/UserPointSubscriptionController.cs
@@ -1,9 +1,12 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using Keylol.Models;
+using Keylol.Models.DTO;
 using Microsoft.AspNet.Identity;
 using Swashbuckle.Swagger.Annotations;
 
@@ -26,6 +29,44 @@ namespace Keylol.Controllers
                 .SelectMany(u => u.SubscribedPoints)
                 .Select(p => p.Id)
                 .ContainsAsync(pointId));
+        }
+
+        /// <summary>
+        /// 获取当前用户订阅的据点
+        /// </summary>
+        /// <param name="skip">起始位置，默认 0</param>
+        /// <param name="take">获取数量，最大 50，默认 30</param>
+        [Route("my")]
+        public async Task<IHttpActionResult> GetMy(int skip = 0, int take = 30)
+        {
+            if (take > 50) take = 50;
+            var userId = User.Identity.GetUserId();
+            var userQuery = DbContext.Users.Where(u => u.Id == userId);
+            return Ok((await userQuery.SelectMany(u => u.SubscribedPoints.OfType<NormalPoint>())
+                .Select(p => new
+                {
+                    user = (KeylolUser) null,
+                    point = p,
+                    articleCount = p.Articles.Count,
+                    subscriberCount = p.Subscribers.Count
+                })
+                .Concat(userQuery.SelectMany(u => u.SubscribedPoints.OfType<ProfilePoint>())
+                    .Select(p => new
+                    {
+                        user = p.User,
+                        point = (NormalPoint) null,
+                        articleCount = p.Entries.OfType<Article>().Count(),
+                        subscriberCount = p.Subscribers.Count
+                    }))
+                .OrderBy(e => e.articleCount)
+                .Skip(() => skip).Take(() => take)
+                .ToListAsync()).Select(e => new SubscribedPointDTO
+                {
+                    User = e.user == null ? null : new UserDTO(e.user),
+                    NormalPoint = e.point == null ? null : new NormalPointDTO(e.point),
+                    ArticleCount = e.articleCount,
+                    SubscribedConut = e.subscriberCount
+                }));
         }
 
         /// <summary>
---
+++ Keylol/Models/DTO/SubscribedPointDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keylol.Models.DTO
+{
+    public class SubscribedPointDTO
+    {
+        public NormalPointDTO NormalPoint { get; set; }
+        public UserDTO User { get; set; }
+        public int? ArticleCount { get; set; }
+        public int? SubscribedConut { get; set; }
+    }
+}
