5ec1e662e9c0883804bfc00c09f98bfc7c6eb48d
Stackie Jia <stackia@keylol.com>
2016-03-23 21:22:27 +0800

    邮政中心数据库建模，新增 Message 等实体（半完成）。

---
+++ Keylol.Models/Message.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Keylol.Models
+{
+    public abstract class Message
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Index]
+        public DateTime CreateTime { get; set; }
+
+        [Required]
+        public string ReceiverId { get; set; }
+
+        public KeylolUser Receiver { get; set; }
+    }
+
+    public class LikeMessage : Message
+    {
+    }
+
+    public class ArticleLikeMessage : Message
+    {
+        public string ArticleId { get; set; }
+        public Article Article { get; set; }
+    }
+
+    public class CommentLikeMessage : LikeMessage
+    {
+        public string CommentId { get; set; }
+        public Comment Comment { get; set; }
+    }
+
+    public abstract class CommentMessage : Message
+    {
+    }
+
+    public class ArticleCommentMessage : CommentMessage
+    {
+        public string ArticleId { get; set; }
+        public Article Article { get; set; }
+    }
+
+    public class CommentReplyMessage : CommentMessage
+    {
+        public string CommentId { get; set; }
+        public Comment Comment { get; set; }
+    }
+
+    public class MissiveMessage : Message
+    {
+    }
+
+    public class ArchiveMissiveMessage : MissiveMessage
+    {
+    }
+
+    public class ArchiveCancelMissiveMessage : MissiveMessage
+    {
+    }
+
+    public class RejectionMissiveMessage : MissiveMessage
+    {
+    }
+
+    public class RejectionCancelMissiveMessage : MissiveMessage
+    {
+    }
+
+    public class SpotlightMissiveMessage : MissiveMessage
+    {
+    }
+
+    public class SpotlightCancelMissiveMessage : MissiveMessage
+    {
+    }
+
+    public class ArticleWarningMissiveMessage : MissiveMessage
+    {
+    }
+
+    public class ArticleWarningCancelMissiveMessage : MissiveMessage
+    {
+    }
+
+    public class CommentWarningMissiveMessage : MissiveMessage
+    {
+    }
+
+    public class CommentWarningCancelMissiveMessage : MissiveMessage
+    {
+    }
+}
