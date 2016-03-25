ecb1ae4a2bf449a584f2aa1e6cd6d5ad2ae60335
Stackie Jia <stackia@keylol.com>
2016-03-25 23:39:05 +0800

    看了超人大战蝙蝠侠。

--- Keylol/Controllers/Message/GetListByCurrentUser.cs
+++ Keylol/Controllers/Message/GetListByCurrentUser.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using Keylol.Models;
 using Microsoft.AspNet.Identity;
 
 namespace Keylol.Controllers.Message
@@ -16,10 +17,41 @@ namespace Keylol.Controllers.Message
         /// </summary>
         [Route]
         [HttpGet]
-        public async Task<IHttpActionResult> GetListByCurrentUser()
+        public async Task<IHttpActionResult> GetListByCurrentUser(MessageFilter? filter = null)
         {
             var userId = User.Identity.GetUserId();
-            return Ok(await DbContext.Messages.ToListAsync());
+            IQueryable<Models.Message> query;
+                switch (filter)
+                {
+                    case MessageFilter.Like:
+                        query = DbContext.Messages.Where(m => m is LikeMessage);
+                        break;
+                    case MessageFilter.Comment:
+                    query = DbContext.Messages.Where(m => m is CommentMessage);
+                    break;
+                    case MessageFilter.Missive:
+                    query = DbContext.Messages.Where(m => m is MissiveMessage);
+                    break;
+                    case null:
+                        query =
+                            DbContext.Messages.Include(m => (m as ArticleArchiveMissiveMessage).Article).AsQueryable();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(filter), filter, null);
+                }
+            var result = await query.ToListAsync();
+            return Ok(result);
+        }
+
+        public enum MessageFilter
+        {
+            Like,
+            Comment,
+            Missive
+        }
+
+        public class MessageGetListByCurrentUserResponseDto
+        {
         }
     }
 }
