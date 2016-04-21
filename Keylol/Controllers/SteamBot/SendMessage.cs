1066e06432eca78df995d09555380556cb8724b0
Stackie Jia <stackia@keylol.com>
2016-04-22 01:33:39 +0800

    可以使用 SteamId 通知机器人给指定用户发消息。 可以设定机器人发消息后两分钟暂时关闭自动回复。

--- Keylol/Controllers/SteamBot/SendMessage.cs
+++ Keylol/Controllers/SteamBot/SendMessage.cs
@@ -3,6 +3,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Web.Http;
 using Keylol.Controllers.User;
 using Keylol.Models;
@@ -19,12 +20,13 @@ namespace Keylol.Controllers.SteamBot
         /// </summary>
         /// <param name="userId">用户 ID，"*" 表示所有机器人给所有好友发送消息</param>
         /// <param name="message">消息内容</param>
-        /// <param name="idType">ID 类型，默认 "Id"</param>
+        /// <param name="tempSilence">是否在两分钟内关闭机器人的自动回复（图灵机器人），对于 userId 为 "*" 的情况无效，默认 false</param>
+        /// <param name="idType">ID 类型，默认 "IdCode"</param>
         [Route("message")]
         [HttpPost]
         [SwaggerResponse(HttpStatusCode.NotFound, "指定用户不存在")]
-        public async Task<IHttpActionResult> SendMessage(string userId, string message,
-            UserController.IdType idType = UserController.IdType.Id)
+        public async Task<IHttpActionResult> SendMessage(string userId, string message, bool tempSilence = false,
+            UserController.IdType idType = UserController.IdType.IdCode)
         {
             if (userId == "*")
             {
@@ -50,6 +52,10 @@ namespace Keylol.Controllers.SteamBot
                         user = await DbContext.Users.SingleOrDefaultAsync(u => u.Id == userId);
                         break;
 
+                    case UserController.IdType.SteamId:
+                        user = await DbContext.Users.SingleOrDefaultAsync(u => u.SteamId == userId);
+                        break;
+
                     default:
                         throw new ArgumentOutOfRangeException(nameof(idType), idType, null);
                 }
@@ -59,6 +65,34 @@ namespace Keylol.Controllers.SteamBot
 
                 if (user.SteamBot.IsOnline())
                 {
+                    if (tempSilence)
+                    {
+                        if (SteamBotCoordinator.AutoChatDisabledBots.ContainsKey(user.SteamBotId))
+                        {
+                            var timer = SteamBotCoordinator.AutoChatDisabledBots[user.SteamBotId];
+                            timer.Stop();
+                            timer.Start();
+                        }
+                        else
+                        {
+                            var timer = new Timer(120000) {AutoReset = false};
+                            timer.Elapsed +=
+                                (sender, args) =>
+                                {
+                                    SteamBotCoordinator.AutoChatDisabledBots.TryRemove(user.SteamBotId, out timer);
+                                };
+                            timer.Start();
+                            SteamBotCoordinator.AutoChatDisabledBots[user.SteamBotId] = timer;
+                        }
+                    }
+                    else if (SteamBotCoordinator.AutoChatDisabledBots.ContainsKey(user.SteamBotId))
+                    {
+                        Timer timer;
+                        if (SteamBotCoordinator.AutoChatDisabledBots.TryRemove(user.SteamBotId, out timer))
+                        {
+                            timer.Stop();
+                        }
+                    }
                     await SteamBotCoordinator.Sessions[user.SteamBot.SessionId]
                         .Client.SendChatMessage(user.SteamBotId, user.SteamId, message, true);
                 }
